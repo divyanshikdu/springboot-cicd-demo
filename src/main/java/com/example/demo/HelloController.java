f205fa86fefade1e5a38980056fd682fa8e176e6
Divyanshi Sengar <divyanshi.sengar@kickdrumtech.com>
2026-02-27 16:25:38 +0530

    ci cd commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/dev")
 @RestController
 public class HelloController {
 
