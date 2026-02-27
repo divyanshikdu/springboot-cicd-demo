2c2d3a5effb8ad061692e164752fb445a66a9302
Divyanshi Sengar <divyanshi.sengar@kickdrumtech.com>
2026-02-27 17:36:59 +0530

    checking

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping
     public String home() {
-        return "CI/CD working successfully ";
+        return "CI/CD working successfully 0";
     }
 
     @GetMapping("/version")
